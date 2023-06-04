065f7581c3a11aa818e31fe881351606875d2e04
Frank Kong <frkong@redhat.com>
2023-06-04 14:06:36 -0400

    modified main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { CssBaseline } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
 
 import App from "./App.tsx";
 import "./index.css";
